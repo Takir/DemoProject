2686c8bc87e5992796083e805bea8ebec7eadb1a
Takir Hossain <taki@takirs-mbp.lan>
2019-09-11 15:47:35 -0400

    komla

---
+++ src/main/java/com/alu/HelloTest.java
@@ -0,0 +1,14 @@
+package com.alu;
+
+import org.testng.annotations.Test;
+
+public class HelloTest {
+	public void test1() {
+		System.out.println("Alu, potol, Begun");
+	}
+	@Test
+	public void sahdba() {
+		test1();
+	}
+
+}
